a76d5b46e65b73958ee6ff207ef4865bce547dee
nattfodd <nattfodd.pp.ua@gmail.com>
2018-05-22 21:48:27 +0300

    Transcryptor via VersionedFields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   include BCrypt
 
   attr_encrypted :ssn,
-    key:       '94dd7e2c40a3d51a8dd0a9137356a18e',
-    algorithm: 'RC2-64-CBC'
+    key:       '67c3800d1572d9d964a6ff3bd821ed02',
+    algorithm: 'aes-256-gcm'
+    # key:       '94dd7e2c40a3d51a8dd0a9137356a18e',
+    # algorithm: 'RC2-64-CBC'
 
   def password
     @password ||= Password.new(password_hash)
